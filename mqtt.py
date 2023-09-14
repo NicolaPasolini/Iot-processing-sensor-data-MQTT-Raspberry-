6dd19ad9cf8962b8c9d62bb73c0610df959dafee
Nicola Pasolini <112755920+NicolaPasolini@users.noreply.github.com>
2023-09-14 13:53:12 +0200

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -4,11 +4,13 @@ from gpiozero import LED
 import time
 
 broker = 'broker.hivemq.com'
-topic = "supertoy/model/tecnobot/serial/pasolini/incarico"
 
-led1 = LED(7)
-led2 = LED(22)
-led3 = LED(18)
+topic = "scatolacompleta"
+topic2 = "spedizionecompleta"
+
+#pin_led = 17
+#led = LED(pin_led)
+#uso led.on() o off() dove mi serve con eventualmente sleep(5) e led.value() per sapere se acceso o spento
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -16,26 +18,27 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect, return code {rc}")
     client.subscribe(topic)
-
+    client.subscribe(topic2)
 
 def on_message(client, userdata, msg):
     messaggio = msg.payload.decode()
     print("Messaggio ricevuto: " + messaggio)
-    #gestione Json
+    #gestione json
     payload = json.loads(msg.payload)
-    n = payload ["num_azioni"]
-    print(n)
-    if(n==1):
-        led1.on()
-        time.sleep(5)
-    elif(n==2):
-        led2.on()
-        time.sleep(5)
-    elif(n==3):
-        led3.on()
-        time.sleep(5)
-    else:
-        print("numero azioni non valido")
+    status1 = payload ["stato_transito"]
+    if(status1==1):
+        print("in transito")
+    elif(status1==0):
+        print("consegnato")
+
+
+def pubblica_messaggio(client, messaggio):
+    client.publish(topic, messaggio)
+
+def createMessage():
+    json_payload={"id_cella": 0, "id_shuttle": 0, "elementi": {"id": 0,"essenza": "a","forma": "b","formato": 0	},"peso": 2, "cod_ordine":0,"stato_transito": 1,"stato_consegnato": 0,"tipo_cella": "c" }
+    return json.dumps(json_payload)
+
 
 
 
@@ -43,9 +46,13 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker, 1883, 60)
+pubblica_messaggio(client, createMessage())
 # Rimani in ascolto dei messaggi sul topic
 client.loop_forever()
 
 
-
-
+def gestisciMsg(msg):
+    payload = json.loads(msg.payload)
+    status1 = payload ["stato_transito"]
+    if(status1==1):
+        print("in transito")
